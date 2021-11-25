b59572d2a71f7047f5db5b701b8ec75c0bb82f59
Bima <bimanugraha732@gmail.com>
2021-11-25 09:15:52 +0700

    Add dockerfile
    Add dockerfile

--- config/database.go
+++ config/database.go
@@ -9,7 +9,9 @@ var DB *gorm.DB
 
 func InitDB() {
 	var err error
-	db, err := gorm.Open(mysql.Open("root:@/ecofriends?parseTime=true"), &gorm.Config{})
+	// db, err := gorm.Open(mysql.Open("root:@/ecofriends?parseTime=true"), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open("root:@tcp(host.docker.internal:3306)/ecofriends?parseTime=true"), &gorm.Config{})
+
 	if err != nil {
 		panic(err)
 	}
